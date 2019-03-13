2ae539dffa165ba94cc49d6a2a725cf5d9a03613
Pinel Raphaël <Raphael.Pinel@etteplanmore.com>
2019-03-13 15:55:09 +0200

    retrieve all books

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books').get((req, res) => {
-  const response = { hello: 'This is my API' };
-  res.json(response);
+  Book.find((err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
 });
 app.get('/', (req, res) => {
   res.send('Welcome to my Nodemon API');
